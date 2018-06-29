8841f7d5297f63c872f686afca4d2f8c001e1d5f
ced <zuger.cedric@gmail.com>
2018-06-29 10:02:54 +0200

    Insertion work.

--- ruby/odyssee.rb
+++ ruby/odyssee.rb
@@ -1,29 +1,22 @@
 require_relative 'google_cal'
 require 'pp'
 
+# This is the list of bases methods for calendars https://www.rubydoc.info/github/google/google-api-ruby-client/Google/Apis/CalendarV3/CalendarService
+
 class Odyssee
 end
 
 gc = GoogleCal.new
 
-# Fetch the next 10 events for the user
-calendar_id = 'primary'
-response = gc.service.list_events(calendar_id,
-                               max_results: 10,
-                               single_events: true,
-                               order_by: 'startTime',
-                               time_min: Time.now.iso8601)
-puts 'Upcoming events:'
-puts 'No upcoming events found' if response.items.empty?
-response.items.each do |event|
-  start = event.start.date || event.start.date_time
-  puts "- #{event.summary} (#{start})"
-end
+start = ( Time.now + 60 ).to_datetime.rfc3339
+end_t = ( Time.now + 120 ).to_datetime.rfc3339
 
-# This is the list of bases methods for calendars https://www.rubydoc.info/github/google/google-api-ruby-client/Google/Apis/CalendarV3/CalendarService
+start_google_format = Google::Apis::CalendarV3::EventDateTime.new( date_time: start )
+end_google_format = Google::Apis::CalendarV3::EventDateTime.new( date_time: end_t )
 
-# pp gc.service.list_calendar_lists
-
-p gc.private_calendar_id
+event = Google::Apis::CalendarV3::Event.new( summary: 'summary test', description: 'description test',
+                                             start: start_google_format, end: end_google_format )
 
 pp gc.service.get_calendar( gc.private_calendar_id )
+
+gc.service.insert_event( gc.private_calendar_id, event )
